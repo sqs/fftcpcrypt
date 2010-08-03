2067f62d2e258c0a53ceeda80254b4d8ba8ebb55
Quinn Slack <qslack@qslack.com>
2010-08-03 10:51:15 -0700

    remove unused prefs and strings

--- defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.fftcpcrypt.boolpref", false);
-pref("extensions.fftcpcrypt.intpref", 0);
-pref("extensions.fftcpcrypt.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.fftcpcrypt@scs.stanford.edu.description", "chrome://fftcpcrypt/locale/overlay.properties");
