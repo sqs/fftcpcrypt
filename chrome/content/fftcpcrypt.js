30e2f4ec0a5004b2c9271f73844351e4929d9071
Quinn Slack <sqs@cs.stanford.edu>
2010-08-19 20:23:55 -0700

    per-browser tracking and display of tcpcrypt session IDs

---
+++ chrome/content/fftcpcrypt.js
@@ -0,0 +1,82 @@
+Components.utils.import("resource://fftcpcrypt/TcpcryptAuthInfoObserver.js");
+
+function FFTcpcrypt() { }
+
+FFTcpcrypt.prototype = {
+  onLoad: function() {
+    // this.strings = document.getElementById("fftcpcrypt-strings");
+    this._authInfoObserver = new TcpcryptAuthInfoObserver(this);
+  },
+
+  get _curTabTcpcryptSessions() {
+      delete this._curTabTcpcryptSessions;
+      return this._curTabTcpcryptSessions = 
+          document.getElementById("tcpcrypt-secinfo-sessid");
+  },
+
+  get _button() {
+    delete this._button;
+    return this._button = document.getElementById("tcpcrypt-toolbar-button");
+  },
+
+  get _popup() {
+    delete this._popup;
+    return this._popup = document.getElementById("tcpcrypt-secinfo-popup");
+  },
+
+
+  //***********************************************************************//
+  // UI Callbacks
+
+  onToolbarButtonCommand: function(event) {
+      var sessions = gBrowser.mCurrentBrowser.tcpcryptSessions;
+      var val = [];
+      for (sessid in sessions) {
+          var urls = sessions[sessid];
+          var urlList = [];
+          for (url in urls)
+              urlList.push(url);
+          val.push(sessid + ": " + urls);
+      }
+      this._curTabTcpcryptSessions.value = val.join("\n");
+      this._popup.openPopup(this._button, "after_end", 25, -10);
+  },
+
+  onPopupShowing: function(event) {
+    // The popupshowing event fires for the menulist too, but we only want
+    // to handle the events for the panel as a whole.
+    if (event.target != this._popup)
+      return;
+  },
+
+
+  //***********************************************************************//
+  // Transport Callbacks
+
+  onTcpcryptSessionEstablished: function (url, sessid) {
+      dump("onTcpcryptSessionEstablished: " +
+           url.spec + ", " + sessid + "\n");
+
+      var browser = gBrowser.mCurrentBrowser;
+      if (!browser.hasOwnProperty('tcpcryptSessions'))
+          browser.tcpcryptSessions = {};
+      if (!browser.tcpcryptSessions.hasOwnProperty(sessid))
+          browser.tcpcryptSessions[sessid] = [];
+
+      var items = browser.tcpcryptSessions[sessid];
+      if (items.indexOf(url.spec) == -1) {
+          browser.tcpcryptSessions[sessid].push(url.spec);
+      }
+  }
+
+};
+
+var fftcpcrypt;
+
+window.addEventListener("load", function() {
+    fftcpcrypt = new FFTcpcrypt();
+    fftcpcrypt.onLoad();
+  }, false);
+window.addEventListener("unload", function() {
+    fftcpcrypt.onUnload();
+  }, false);
--- modules/TcpcryptAuthInfoObserver.js
+++ modules/TcpcryptAuthInfoObserver.js
@@ -1,33 +1,108 @@
 const EXPORTED_SYMBOLS = ['TcpcryptAuthInfoObserver'];
 
-function TcpcryptAuthInfoObserver() {
+
+function setTimeout(func, delay)
+{
+var timer =
+Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
+var callback = { that: this, notify: function () {
+func.call(this.that); } };
+timer.initWithCallback(callback, delay,
+Components.interfaces.nsITimer.TYPE_ONE_SHOT);
+return timer;
+}
+
+
+// reference to the interface defined in nsIHttpActivityObserver.idl
+const nsIHttpActivityObserver = Components.interfaces.nsIHttpActivityObserver;
+const nsISocketTransport = Components.interfaces.nsISocketTransport;
+
+// reference to the required base interface that all components must support
+const nsISupports = Components.interfaces.nsISupports;
+
+function TcpcryptAuthInfoObserver(fftcpcrypt) {
+  let c = "@mozilla.org/network/http-activity-distributor;1";
+  let activityDistributor = Components.classes[c]
+                            .getService(Components.interfaces.nsIHttpActivityDistributor);
+  activityDistributor.addObserver(this);
+  this._fftcpcrypt = fftcpcrypt;
+
   this._log("TcpcryptAuthInfoObserver() \n\n");
 }
 
 TcpcryptAuthInfoObserver.prototype = {
   _log: function(s) {
-    dump("TcpcryptAuthInfoObserver: " + s + "\n");
+    dump("-- TcpcryptAuthInfoObserver: " + s + "\n");
   },
 
-  observe: function(subject, topic, data) {
-    if (topic == "http-on-examine-response") {
-      var httpChannel = subject.QueryInterface(Components.interfaces.nsIHttpChannel);
-      dump("-----------------------------------\n\n\n");
-      this._log("securityInfo = " + httpChannel.securityInfo.toSource());
+  observeActivity: function(channel, aActivityType, aActivitySubtype,
+                            aTimestamp, aExtraSizeData, aExtraStringData)
+  {
+      if (true || aActivitySubtype == nsIHttpActivityObserver.ACTIVITY_SUBTYPE_RESPONSE_COMPLETE) {
+        //setTimeout(function(){
+        channel.QueryInterface(Components.interfaces.nsIChannel);
+        dump(">>>> " + acType(aActivityType) + " : " + 
+             acSubType(aActivitySubtype) + "\n:::: " +
+             channel.URI.spec + "\n");
+        if (channel.securityInfo) {
+            let tcI = Components.interfaces.tcITransportSessionInfo;
+            let tc  = channel.securityInfo.QueryInterface(tcI);
+            dump("--------------------------------------------------\n"+
+                 channel.URI.hostPort + ": " + 
+                 "TCP_CRYPT_SESSID = " + tc.sessionID + "\n" +
+                 "--------------------------------------------------\n");
+            this._fftcpcrypt.onTcpcryptSessionEstablished(channel.URI, tc.sessionID);
+            //},1);
+        } else {
+            dump("securityInfo is undefined\n");
+        }
     }
-  },
-
-  get observerService() {
-    return Components.classes["@mozilla.org/observer-service;1"]
-                     .getService(Components.interfaces.nsIObserverService);
-  },
-
-  register: function() {
-    this.observerService.addObserver(this, "http-on-examine-response", false);
-  },
-
-  unregister: function() {
-    this.observerService.removeObserver(this, "http-on-examine-response");
-  }
-
+  } 
 };
+
+function acType(a)
+{
+  switch (a) {
+    case nsIHttpActivityObserver.ACTIVITY_TYPE_SOCKET_TRANSPORT:
+      return "ACTIVITY_TYPE_SOCKET_TRANSPORT";
+    case nsIHttpActivityObserver.ACTIVITY_TYPE_HTTP_TRANSACTION:
+      return "ACTIVITY_TYPE_HTTP_TRANSACTION";
+      
+    default:
+      return a;
+  }
+}
+
+function acSubType(a)
+{
+  switch (a) {
+    case nsIHttpActivityObserver.ACTIVITY_SUBTYPE_REQUEST_HEADER:
+      return "ACTIVITY_SUBTYPE_REQUEST_HEADER";
+    case nsIHttpActivityObserver.ACTIVITY_SUBTYPE_REQUEST_BODY_SENT:
+      return "ACTIVITY_SUBTYPE_REQUEST_BODY_SENT";
+    case nsIHttpActivityObserver.ACTIVITY_SUBTYPE_RESPONSE_START:
+      return "ACTIVITY_SUBTYPE_RESPONSE_START";
+    case nsIHttpActivityObserver.ACTIVITY_SUBTYPE_RESPONSE_HEADER:
+      return "ACTIVITY_SUBTYPE_RESPONSE_HEADER";
+    case nsIHttpActivityObserver.ACTIVITY_SUBTYPE_RESPONSE_COMPLETE:
+      return "ACTIVITY_SUBTYPE_RESPONSE_COMPLETE";
+    case nsIHttpActivityObserver.ACTIVITY_SUBTYPE_TRANSACTION_CLOSE:
+      return "ACTIVITY_SUBTYPE_TRANSACTION_CLOSE";
+      
+    case nsISocketTransport.STATUS_RESOLVING:
+      return "STATUS_RESOLVING";
+    case nsISocketTransport.STATUS_CONNECTING_TO:
+      return "STATUS_CONNECTING_TO";
+    case nsISocketTransport.STATUS_CONNECTED_TO:
+      return "STATUS_CONNECTED_TO";
+    case nsISocketTransport.STATUS_SENDING_TO:
+      return "STATUS_SENDING_TO";
+    case nsISocketTransport.STATUS_WAITING_FOR:
+      return "STATUS_WAITING_FOR";
+    case nsISocketTransport.STATUS_RECEIVING_FROM:
+      return "STATUS_RECEIVING_FROM";    
+      
+    default:
+      return a;
+  }
+}
